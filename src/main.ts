50985c3c384c3e31445077ea97ca3316739e0139
Shoodey <shoodey@gmail.com>
2025-02-21 02:01:44 +0200

    parse inputs & context

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
-const foo = () => {
-    console.debug("Hello from foo!");
-};
+import { getInput } from "@actions/core";
+import { context } from "@actions/github";
 
-foo();
+export function run() {
+    const githubToken = getInput("GITHUB_TOKEN", { required: true });
+    const jiraProjectKey = getInput("JIRA_PROJECT_KEY", { required: true });
+    const jiraTicketPlaceholder = getInput("JIRA_TICKET_PLACEHOLDER");
+
+    const payload = context.payload;
+
+    console.debug({
+        githubToken,
+        jiraProjectKey,
+        jiraTicketPlaceholder,
+        payload,
+    });
+}
